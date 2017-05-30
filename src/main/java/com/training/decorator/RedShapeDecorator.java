671b269e870d1d59fef2d729562c469daa074e36
Adam Reid <adameastcoast@gmail.com>
2017-05-30 01:22:42 -0400

    Add red shape decorator

---
+++ src/main/java/com/training/decorator/RedShapeDecorator.java
@@ -0,0 +1,21 @@
+package com.training.decorator;
+
+/**
+ * Created by adame on 5/30/2017.
+ */
+public class RedShapeDecorator extends ShapeDecorator{
+
+    public RedShapeDecorator(Shape decoratedShape) {
+        super(decoratedShape);
+    }
+
+    @Override
+    public void draw() {
+        decoratedShape.draw();
+        setRedBorder(decoratedShape);
+    }
+
+    private void setRedBorder(Shape decoratedShape) {
+        System.out.println("The border is red!");
+    }
+}
